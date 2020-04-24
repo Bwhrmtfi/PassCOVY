02ccd3e6803a552a9c752abc1a39ce8d7144562c
Trinh Thanh Phu <thanhphua4tcv@gmail.com>
2020-04-24 19:21:11 +0700

    first commit

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'HOST_NAME'     => 'localhost',
-    'DATABASE_NAME' => 'pass_covy',
-    'USERNAME'      => 'root',
-    'PASSWORD'      => ''
+    'HOST_NAME'     => 'g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'DATABASE_NAME' => 'g19jh2rky4ccw9xc',
+    'USERNAME'      => 'rej8hh5anqviry98',
+    'PASSWORD'      => 'ewnzhbaatc83un8z'
 );
